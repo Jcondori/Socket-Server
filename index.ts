9ba4488597312818ee4f5a3650c5d9cb09c8d0e1
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-01-25 19:38:04 -0500

    Chat

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router from "./routes/router";
 import bodyParser from "body-parser";
 import cors from "cors"
 
-const server = new Server();
+const server = Server.instance;
 
 // Body Parser
 server.app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +13,7 @@ server.app.use(bodyParser.json());
 server.app.use(cors({origin: true, credentials: true}));
 
 // Rutas de Servicio
-server.app.use('/', router)
+server.app.use('/', router);
 
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port}`)
